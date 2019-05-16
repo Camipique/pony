007b0b44e4592282d3c061d2e1473811a34353b8
Carlos Quendera <c.quendera@campus.fct.unl.pt>
2019-05-16 15:50:07 +0100

    Updated cosmos test 2.0

--- pony/orm/tests/teste_cosmos2.py
+++ pony/orm/tests/teste_cosmos2.py
@@ -23,12 +23,6 @@ class Group(db.Entity):
 
 db.generate_mapping(create_tables=True)
 
-with db_session:
-    g1 = Group(number=1)
-    Student(id=1, name='S1', group=g1, gpa=3.1)
-    Student(id=2, name='S2', group=g1, gpa=3.2, scholarship=100, dob=date(2000, 1, 1))
-    Student(id=3, name='S3', group=g1, gpa=3.3, scholarship=200, dob=date(2001, 1, 2))
-
 
 class TestQuery(unittest.TestCase):
     def setUp(self):
@@ -39,9 +33,31 @@ class TestQuery(unittest.TestCase):
         rollback()
         db_session.__exit__()
 
+
     def test1(self):
-        result = Group.select()[:]
-        self.assertIsNotNone(result)
+        with db_session:
+            g1 = Group(number=1)
+            s1 = Student(id=1, name='S1', group=g1, gpa=3.1)
+            s2 = Student(id=2, name='S2', group=g1, gpa=3.2, scholarship=100, dob=date(2000, 1, 1))
+            s3 = Student(id=3, name='S3', group=g1, gpa=3.3, scholarship=200, dob=date(2001, 1, 2))
+            
+            result = Group.select()[:]
+            self.assertIsNotNone(result)
+            group1 = result[0]
+            self.assertEqual(group1.number, g1.number)
+
+            result = Student.select()[:]
+            self.assertIsNotNone(result)
+            student1 = result[0]
+            student2 = result[1]
+            student3 = result[2]
+            self.assertEqual(student1.id, s1.id)
+            self.assertEqual(student1.name, s1.name)
+            self.assertEqual(student1.scholarship, s1.scholarship)
+            self.assertEqual(student1.gpa, s1.gpa)
+            self.assertEqual(student1.group, s1.group)
+            self.assertEqual(student1.dob, s1.dob)
+
 
 if __name__ == '__main__':
     unittest.main()

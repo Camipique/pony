c5aea53ab61378d40cade7b020b56ed8489e91a2
Carlos Quendera <c.quendera@campus.fct.unl.pt>
2019-05-22 15:39:40 +0100

    Test queries using pk

--- pony/orm/tests/teste_cosmos2.py
+++ pony/orm/tests/teste_cosmos2.py
@@ -1,5 +1,4 @@
 from __future__ import absolute_import, print_function, division
-from pony.py23compat import PYPY2, pickle
 
 import unittest
 from datetime import date
@@ -10,16 +9,20 @@ from pony.orm.tests.testutils import *
 
 db = Database('cosmosdb', 'https://localhost:8081', 'C2y6yDjf5/R+ob0N8A7Cgv30VRDJIWEHLM+4QDU5DE2nQ9nDuVTqobD4b8mGGyPMbIZnqyMsEcaGQy67XIw/Jw==', 'SchoolDatabase', 'School')
 
+
 class Student(db.Entity):
+    id = PrimaryKey(int)
     name = Required(unicode)
     scholarship = Optional(int)
-    gpa = Optional(Decimal,3,1)
+    gpa = Optional(Decimal, 3, 1)
     group = Required('Group')
     dob = Optional(date)
+
 
 class Group(db.Entity):
     number = PrimaryKey(int)
     students = Set(Student)
+
 
 db.generate_mapping(create_tables=True)
 
@@ -33,30 +36,73 @@ class TestQuery(unittest.TestCase):
         rollback()
         db_session.__exit__()
 
-
     def test1(self):
         with db_session:
             g1 = Group(number=1)
             s1 = Student(id=1, name='S1', group=g1, gpa=3.1)
             s2 = Student(id=2, name='S2', group=g1, gpa=3.2, scholarship=100, dob=date(2000, 1, 1))
             s3 = Student(id=3, name='S3', group=g1, gpa=3.3, scholarship=200, dob=date(2001, 1, 2))
-            
+
+            # Select all groups
             result = Group.select()[:]
             self.assertIsNotNone(result)
+
             group1 = result[0]
+
             self.assertEqual(group1.number, g1.number)
 
+            # Select all students
             result = Student.select()[:]
             self.assertIsNotNone(result)
+
             student1 = result[0]
             student2 = result[1]
             student3 = result[2]
-            self.assertEqual(student1.id, s1.id)
-            self.assertEqual(student1.name, s1.name)
-            self.assertEqual(student1.scholarship, s1.scholarship)
-            self.assertEqual(student1.gpa, s1.gpa)
-            self.assertEqual(student1.group, s1.group)
-            self.assertEqual(student1.dob, s1.dob)
+
+            self.assertEqual(s1, student1)
+            self.assertEqual(s2, student2)
+            self.assertEqual(s3, student3)
+
+            # Simple select with conditions
+            result = Group.select(lambda g: g.number == 1)[:]
+            self.assertIsNotNone(result)
+
+            for g in result:
+                self.assertEqual(g.number, 1)
+
+            result = Student.select(lambda s: s.gpa > Decimal('3.1'))[:]
+            self.assertIsNotNone(result)
+
+            for s in result:
+                self.assertGreater(s.gpa, 3.1)
+
+            def get_group_by_pk(_id):
+                return Group[_id]
+
+            def get_student_by_pk(_id):
+                return Student[_id]
+
+            # Get student object by primary key
+            result = get_student_by_pk(2)
+            self.assertIsNotNone(result)
+
+            self.assertEqual(s2, result)
+
+            # Try to get a non existent student, using primary key
+            self.assertRaises(pony.orm.core.ObjectNotFound, get_student_by_pk, 4)
+
+            # Get group object by primary key
+            result = get_group_by_pk(1)
+            self.assertIsNotNone(result)
+
+            self.assertEqual(g1, result)
+
+            # Try to get a non existent group, using primary key
+            self.assertRaises(pony.orm.core.ObjectNotFound, get_group_by_pk, 2)
+
+            result = Student.select(lambda s: s.group.number == g1.number)
+
+            #
 
 
 if __name__ == '__main__':
